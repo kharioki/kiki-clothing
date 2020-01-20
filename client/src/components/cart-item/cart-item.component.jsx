05473c42d555393ac73d067b8280f76171c760d6
kharioki <antolisah@gmail.com>
2020-01-20 14:59:09 +0300

    added performance optimization with react.memo

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { CartItemContainer, CartItemImage, ItemDetailsContainer } from './cart-item.styles';
+import {
+  CartItemContainer,
+  CartItemImage,
+  ItemDetailsContainer
+} from "./cart-item.styles";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-    <CartItemContainer>
-        <CartItemImage src={imageUrl} alt='item' />
-        <ItemDetailsContainer>
-            <span>{name}</span>
-            <span>{quantity} x ${price}</span>
-        </ItemDetailsContainer>
-    </CartItemContainer>
+  <CartItemContainer>
+    <CartItemImage src={imageUrl} alt="item" />
+    <ItemDetailsContainer>
+      <span>{name}</span>
+      <span>
+        {quantity} x ${price}
+      </span>
+    </ItemDetailsContainer>
+  </CartItemContainer>
 );
 
-export default CartItem;
+export default React.memo(CartItem);
