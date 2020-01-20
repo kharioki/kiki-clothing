a62103c80e2a3928804be3e2d2be6c1daed1015f
kharioki <antolisah@gmail.com>
2020-01-20 12:56:14 +0300

    added mobile responsiveness using media queries

--- client/src/components/checkout-item/checkout-item.styles.jsx
+++ client/src/components/checkout-item/checkout-item.styles.jsx
@@ -1,47 +1,55 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CheckoutItemContainer = styled.div`
-    width: 100%;
-    display: flex;
-    min-height: 100px;
-    border-bottom: 1px solid darkgrey;
-    padding: 15px 0;
-    font-size: 20px;
-    align-items: center;
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+
+  @media screen and (max-width: 800px) {
+    font-size: 18px;
+  }
 `;
 
 export const CheckoutImageContainer = styled.div`
-    width: 23%;
-    padding-right: 15px;
+  width: 23%;
+  padding-right: 15px;
 `;
 
 export const CheckoutImage = styled.img`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
 export const NameContainer = styled.span`
-    width: 23%;
+  width: 23%;
+
+  @media screen and (max-width: 800px) {
+    width: 22%;
+  }
 `;
 
 export const QuantityContainer = styled.span`
-    width: 23%;
-    display: flex;
+  width: 23%;
+  display: flex;
 `;
 
 export const ArrowContainer = styled.div`
-    cursor: pointer;
+  cursor: pointer;
 `;
 
 export const ValueContainer = styled.span`
-    margin: 0 10px;
+  margin: 0 10px;
 `;
 
 export const PriceContainer = styled.span`
-    width: 23%;
+  width: 23%;
 `;
 
 export const RemoveButtonContainer = styled.div`
-    padding-left: 12px;
-    cursor: pointer;
+  padding-left: 12px;
+  cursor: pointer;
 `;
--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const MenuItemContainer = styled.div`
   min-width: 30%;
-  height: ${({ size }) => (size ? '380px' : '240px')};
+  height: ${({ size }) => (size ? "380px" : "240px")};
   flex: 1 1 auto;
   display: flex;
   align-items: center;
@@ -13,23 +13,27 @@ export const MenuItemContainer = styled.div`
 
   &:hover {
     cursor: pointer;
-  
+
     & .background-image {
       transform: scale(1.1);
       transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
     }
-  
+
     & .content {
       opacity: 0.9;
     }
   }
-  
+
   &:first-child {
     margin-right: 7.5px;
   }
 
   &:last-child {
     margin-left: 7.5px;
+  }
+
+  @media screen and (max-width: 800px) {
+    height: 200px;
   }
 `;
 
